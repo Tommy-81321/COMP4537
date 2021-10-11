1acbd8d8dff8e5f49aed2701197ccd403594a8da
pkaur740 <panesarpardeep0@gmail.com>
2021-10-10 21:35:25 -0700

    made some minor changes

--- labs/3/server.js
+++ labs/3/server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var url = require('url');
-const dateTime = require('./utils');
+const dateTime = require('./modules/utils');
 var PORT = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var name = q.name;
-  const string = "Hello " +  name + " What a beautiful day. Server current date and time is " + dateTime.myDateTime();
+  let q = url.parse(req.url, true).query;
+  let name = q.name;
+  const string = "Hello " +  name + ", What a beautiful day. Server current date and time is " + dateTime.myDateTime();
   res.end("<p style='color:blue'>" + string + "</p>");
 }).listen(PORT);
